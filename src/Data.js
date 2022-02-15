370d477487c8a5bd7417fadbe586ea20d748cec4
iokl1004 <iokl3319@naver.com>
2022-02-15 16:02:45 +0900

    Update Data.js

--- src/Data.js
+++ src/Data.js
@@ -5,31 +5,31 @@ const postList = [
         "no": 1,
         "title": "첫번째 게시글입니다.",
         "content": "첫번째 게시글 내용입니다.",
-        "CreateDate": "2020-10-25",
+        "CreateDate": "2022-02-15",
         "readCount": 6
     }, {
         "no": 2,
         "title": "두번째 게시글입니다.",
         "content": "두번째 게시글 내용입니다.",
-        "CreateDate": "2020-10-25",
+        "CreateDate": "2022-02-15",
         "readCount": 5
     }, {
         "no": 3,
         "title": "세번째 게시글입니다.",
         "content": "세번째 게시글 내용입니다.",
-        "CreateDate": "2020-10-25",
+        "CreateDate": "2022-02-15",
         "readCount": 1
     }, {
         "no": 4,
         "title": "네번째 게시글입니다.",
         "content": "네번째 게시글 내용입니다.",
-        "CreateDate": "2020-10-25",
+        "CreateDate": "2022-02-15",
         "readCount": 6
     }, {
         "no": 5,
         "title": "다섯번째 게시글입니다.",
         "content": "다섯번째 게시글 내용입니다.",
-        "CreateDate": "2020-10-25",
+        "CreateDate": "2022-02-15",
         "readCount": 4
     },
 ];
